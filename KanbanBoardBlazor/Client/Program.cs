ee0ae5a9419d282cc6167343670cb564b69ceaa7
jimmys20 <maragkosd12@gmail.com>
2020-11-30 16:26:05 +0200

    commit

--- KanbanBoardBlazor/Client/Program.cs
+++ KanbanBoardBlazor/Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -19,6 +20,7 @@ namespace KanbanBoardBlazor.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
     }
