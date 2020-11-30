1264904adb7395bb201faed2c06077d1d2fa7a05
jimmys20 <maragkosd12@gmail.com>
2020-11-30 16:41:46 +0200

    commit

---
+++ KanbanBoardBlazor/Shared/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoardBlazor.Shared
+{
+    public class Project
+    {
+        public List<Stage> stages { get; set; } = new List<Stage>();
+    }
+}
---
+++ KanbanBoardBlazor/Shared/Stage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoardBlazor.Shared
+{
+    public class Stage
+    {
+        public string title { get; set; }
+
+        public List<Issue> issues { get; set; } = new List<Issue>();
+    }
+}
